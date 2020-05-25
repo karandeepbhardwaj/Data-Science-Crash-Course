e248539a5582ad9608c7514e1b553cd95bc1da27
Karandeep Bhardwaj <karandeepbhardwaj@Karandeeps-MacBook-Pro.local>
2020-05-24 23:21:52 -0400

    initial commit

---
+++ Crash course/barChart1.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+
+movies = ["Annie Hall", "Ben-hurl", "casablanca", "Gandhi", "West Side Story"]
+num_oscars = [5, 11, 3, 8, 10]
+
+plt.bar(range(len(movies)), num_oscars)
+plt.title("My Favourite movies")
